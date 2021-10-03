108029efa826d788adae964cee16dda6eb489954
kanhaiya tulsyan <kanhaiyatulsyan@gmail.com>
2021-10-03 17:31:29 +0530

    Query done

--- app/src/main/java/com/example/instafire/ProfileActivity.kt
+++ app/src/main/java/com/example/instafire/ProfileActivity.kt
@@ -3,14 +3,20 @@ package com.example.instafire
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
